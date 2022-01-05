128cc32f6b875f0354581aa96c8ccc18ad6313ce
Arvitrik <90417427+Arvitrik@users.noreply.github.com>
2022-01-05 22:39:35 +0200

    Add files via upload

--- script/SetA.js
+++ script/SetA.js
@@ -1,12 +1,16 @@
-// Task 1 - Color Flipper
+//...................................................................
+//.....................  Task 1 - Color Flipper .....................
+//...................................................................
 
 // UI Components
+
 const textColor = document.querySelector(".span-color__cF");
 const btn = document.querySelector(".btn__cF");
 const bg = document.querySelector(".set-1");
 const links = document.querySelectorAll(".cFOption");
 
 // Color Selectors
+
 const simple = [
   "aliceblue",
   "antiquewhite",
@@ -22,41 +26,15 @@ const simple = [
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f"];
 const rgb = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
-// Check for type of output - hex, rgb or simple
-links.forEach((link) => {
-  //   console.log(link); --- Testing for values
-  link.addEventListener("click", (evt) => {
-    let evtCL = evt.currentTarget.classList;
-    // console.log(evtCL); --- Testing for values
-    evtCL.add("active");
-  });
-});
-
-// Event Listener for the Button to generate colors
-btn.addEventListener("click", () => {
-  links.forEach((link) => {
-    if (link.classList.contains("active")) {
-      //   console.log(link);
-      if (link.classList.contains("hexCF")) {
-        hexColor();
-      } else if (link.classList.contains("rgbCF")) {
-        rgbColor();
-      } else {
-        simpleColor();
-      }
-    } else {
-      simpleColor();
-    }
-  });
-});
-
 // Function Declarations
+
 function hexColor() {
   let colr = "#";
-  for (i = 0; i < 6; i++) {
+  for (i = 0; i <= 5; i++) {
     colr += hex[randomColr(hex)];
   }
   textColor.textContent = colr;
+  // console.log(colr, textColor);
   bg.style.backgroundColor = colr;
 }
 
@@ -82,3 +60,34 @@ let randomColr = (input) => {
     return Math.floor(Math.random() * input.length);
   }
 };
+
+// Output format - hex, rgb or simple (default)
+
+function optionActive(evt) {
+  for (let i = 0; i < links.length; i++) {
+    links[i].classList.remove("active");
+    if (links[i].classList === evt.classList) evt.classList.add("active");
+  }
+}
+
+// Just click on button
+
+btn.addEventListener("click", (evt) => {
+  evt.preventDefault(); // To prevent page refresh
+  links.forEach((link) => {
+    if (link.classList.contains("active")) {
+      //   console.log(link, link.classList);
+      if (link.classList.contains("hexCF")) {
+        hexColor();
+      } else if (link.classList.contains("rgbCF")) {
+        rgbColor();
+      } else if (link.classList.contains("simpleCF")) {
+        simpleColor();
+      }
+    }
+  });
+});
+
+//...................................................................
+//.....................  Task 2 - Color Flipper .....................
+//...................................................................
